1144012009603c6b2fa8154336022c14d8877a3d
Ricardo Brunetto <ricardo@rhbrunetto.com>
2023-09-07 03:43:01 -0300

    upd

--- lib/core/authenticated_flow.dart
+++ lib/core/authenticated_flow.dart
@@ -2,6 +2,7 @@ import 'package:dfunc/dfunc.dart';
 import 'package:eth_chat/features/chat/module.dart';
 import 'package:eth_chat/features/session/data/session.dart';
 import 'package:eth_chat/features/session/services/session_cubit.dart';
+import 'package:eth_chat/features/xmtp/module.dart';
 import 'package:eth_chat/routes.gr.dart';
 import 'package:flutter/material.dart';
 import 'package:auto_route/auto_route.dart';
@@ -23,6 +24,7 @@ class AuthenticatedFlowScreen extends StatelessWidget {
           return MultiProvider(
             providers: [
               Provider<Session>.value(value: account),
+              const XmtpModule(),
               const ChatModule(),
             ],
             child: const AutoRouter(),
--- lib/di.config.dart
+++ lib/di.config.dart
@@ -9,14 +9,17 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:eth_chat/db/db.dart' as _i3;
-import 'package:eth_chat/features/chat/data/chat_repository.dart' as _i6;
-import 'package:eth_chat/features/chat/services/chat_service.dart' as _i7;
-import 'package:eth_chat/features/chat/services/chat_watcher.dart' as _i8;
-import 'package:eth_chat/features/chat/services/convo_service.dart' as _i9;
-import 'package:eth_chat/features/session/services/session_cubit.dart' as _i4;
+import 'package:eth_chat/db/db.dart' as _i4;
+import 'package:eth_chat/di.dart' as _i12;
+import 'package:eth_chat/features/chat/data/chat_repository.dart' as _i8;
+import 'package:eth_chat/features/chat/services/chat_service.dart' as _i9;
+import 'package:eth_chat/features/chat/services/chat_watcher.dart' as _i10;
+import 'package:eth_chat/features/chat/services/convo_service.dart' as _i11;
+import 'package:eth_chat/features/session/services/session_cubit.dart' as _i5;
 import 'package:eth_chat/features/wallet_connect/services/wallet_bloc.dart'
-    as _i5;
+    as _i6;
+import 'package:eth_chat/features/xmtp/services/xmtp_service.dart' as _i7;
+import 'package:flutter_secure_storage/flutter_secure_storage.dart' as _i3;
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
@@ -31,30 +34,36 @@ extension GetItInjectableX on _i1.GetIt {
       environment,
       environmentFilter,
     );
-    gh.lazySingleton<_i3.MyDatabase>(() => _i3.MyDatabase());
-    gh.factory<_i4.SessionCubit>(() => _i4.SessionCubit());
-    gh.factory<_i5.WalletBloc>(() => _i5.WalletBloc());
-    gh.factory<_i6.ChatRepository>(
-        () => _i6.ChatRepository(gh<_i3.MyDatabase>()));
-    gh.factoryParam<_i7.ChatService, String, dynamic>((
+    final appModule = _$AppModule();
+    gh.lazySingleton<_i3.FlutterSecureStorage>(() => appModule.secureStorage);
+    gh.lazySingleton<_i4.MyDatabase>(() => _i4.MyDatabase());
+    gh.factory<_i5.SessionCubit>(() => _i5.SessionCubit());
+    gh.factory<_i6.WalletBloc>(() => _i6.WalletBloc());
+    gh.factory<_i7.XmtpService>(
+        () => _i7.XmtpService(storage: gh<_i3.FlutterSecureStorage>()));
+    gh.factory<_i8.ChatRepository>(
+        () => _i8.ChatRepository(gh<_i4.MyDatabase>()));
+    gh.factoryParam<_i9.ChatService, String, dynamic>((
       sender,
       _,
     ) =>
-        _i7.ChatService(
+        _i9.ChatService(
           sender: sender,
-          repository: gh<_i6.ChatRepository>(),
+          repository: gh<_i8.ChatRepository>(),
         ));
-    gh.factory<_i8.ChatWatcher>(
-        () => _i8.ChatWatcher(repository: gh<_i6.ChatRepository>()));
-    gh.factoryParam<_i9.ConvoService, String, String>((
+    gh.factory<_i10.ChatWatcher>(
+        () => _i10.ChatWatcher(repository: gh<_i8.ChatRepository>()));
+    gh.factoryParam<_i11.ConvoService, String, String>((
       sender,
       recipient,
     ) =>
-        _i9.ConvoService(
+        _i11.ConvoService(
           sender: sender,
           recipient: recipient,
-          repository: gh<_i6.ChatRepository>(),
+          repository: gh<_i8.ChatRepository>(),
         ));
     return this;
   }
 }
+
+class _$AppModule extends _i12.AppModule {}
